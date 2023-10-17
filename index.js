896e564cec60ad8237248097b37ead4766786b6e
MariaMantusova <maria.mantusova@mail.ru>
2023-10-17 13:13:22 +0300

    feat: delete cors

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use('/', (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
+  // if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-  }
+  // }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
