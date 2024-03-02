4ef5e7328fb6c1e202bb9c821ac76a0ee859c915
MariaMantusova <maria.mantusova@mail.ru>
2024-03-02 18:07:45 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const limiter = rateLimit({
 const PORT = 5000;
 
 const allowedCors = [
-  '*',
   'https://healthywoman-next.vercel.app',
 ];
 
@@ -27,10 +26,10 @@ app.use('/', (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  // if (allowedCors.includes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-  // }
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
